79cf49882cb15d93dbea2f4a5f7633a2ced22004
Eduard Besjentsev <eduard.besjentsev@student.uantwerpen.be>
2016-11-16 21:15:44 +0100

    robuster tests

--- scripts/IGMPv3/tests/joins/report-read-states.sh
+++ scripts/IGMPv3/tests/joins/report-read-states.sh
@@ -9,30 +9,20 @@ sleep 1
 # Join / empty source list 
 echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.1.1.1, FILTER EXCLUDE"
 
-# INCLUDE multiple sources
-# echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.2.2.2, FILTER INCLUDE, SRC 1.1.1.1, SRC 2.2.2.2, SRC 3.3.3.3"
-
+# INCLUDE multiple sources, tests union
 echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.2.2.2, FILTER INCLUDE, SRC 3.3.3.3"
 echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.2.2.2, FILTER INCLUDE, SRC 2.2.2.2"
 echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.2.2.2, FILTER INCLUDE, SRC 1.1.1.1"
 
+# EXCLUDE multiple sources, tests intersection
+echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.3.3.3, FILTER EXCLUDE, SRC 1.1.1.1, SRC 2.2.2.2, SRC 3.3.3.3"
+echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.3.3.3, FILTER EXCLUDE, SRC 2.2.2.2, SRC 3.3.3.3"
 
-
-
-# update to fewer SRC addresses
-echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.3.3.3, FILTER EXCLUDE, SRC 1.1.1.1, SRC 2.2.2.2"
-echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.3.3.3, FILTER EXCLUDE, SRC 1.1.1.1"
-
-# update to more SRC addresses
-echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.4.4.4, FILTER EXCLUDE, SRC 1.1.1.1"
-echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.4.4.4, FILTER EXCLUDE, SRC 1.1.1.1, SRC 2.2.2.2, SRC 3.3.3.3"
-
-# different socket
-echo "write reporter.join_group PORT 2222, INTERFACE 0, GROUP 225.4.4.4, FILTER EXCLUDE, SRC 2.2.2.2, SRC 3.3.3.3"
-
-# different socket + update to INCLUDE mode
-echo "write reporter.join_group PORT 3333, INTERFACE 0, GROUP 225.4.4.4, FILTER EXCLUDE, SRC 1.1.1.1, SRC 2.2.2.2"
-echo "write reporter.join_group PORT 3333, INTERFACE 0, GROUP 225.4.4.4, FILTER INCLUDE, SRC 3.3.3.3, SRC 4.4.4.4, SRC 5.5.5.5"
+# different socket, tests difference over all sockets
+echo "write reporter.join_group PORT 2222, INTERFACE 0, GROUP 225.4.4.4, FILTER INCLUDE, SRC 3.3.3.3"
+# different socket + INCLUDE mode, tests difference within socket and whether the state keeps EXCLUDE filter mode
+echo "write reporter.join_group PORT 3333, INTERFACE 0, GROUP 225.4.4.4, FILTER EXCLUDE, SRC 2.2.2.2, SRC 3.3.3.3, SRC 4.4.4.4"
+echo "write reporter.join_group PORT 3333, INTERFACE 0, GROUP 225.4.4.4, FILTER INCLUDE, SRC 4.4.4.4, SRC 5.5.5.5"
 
 # Join group and immediately leave it
 echo "write reporter.join_group PORT 1111, INTERFACE 0, GROUP 225.6.6.6, FILTER EXCLUDE, SRC 1.1.1.1"
